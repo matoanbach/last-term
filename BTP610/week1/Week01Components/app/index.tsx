5f92c0eb069369598fb203e87bb75b882a464785
matoanbach <bachmatoan2910@gmail.com>
2026-06-16 22:48:57 -0400

    week1: refactor form screen with styles

--- BTP610/week1/Week01Components/app/index.tsx
+++ BTP610/week1/Week01Components/app/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Text, View, Image, TextInput } from "react-native";
+import { Text, View, Image, TextInput, StyleSheet } from "react-native";
 import { Dropdown } from 'react-native-element-dropdown';
 import { Checkbox } from 'expo-checkbox';
 
@@ -10,13 +10,11 @@ import { Checkbox } from 'expo-checkbox';
 // npx expo install expo-checkbox
 
 export default function Index() {
-  // Logic/Functional goes here
-  const [name, setName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [selectedProgram, setSelectedProgram] = useState<string>("Cloud Computing");
-  const [selectedCampuses, setSelectedCampuses] = useState<string[]>(["Newnham", "King"]);
-  const [selectedTution, setSelectedTution] = useState<string>("FullTime");
+  const [name, setName] = useState<string>("")
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [selectedProgram, setSelectedProgram] = useState<string>("Cloud Computing")
+  const [selectedCampuses, setSelectedCampuses] = useState<string[]>([])
 
   const programs: string[] = [
     "AI",
@@ -26,153 +24,146 @@ export default function Index() {
   ]
 
   const campusOptions: string[] = ["Newnham", "King", "York"]
-  const tutionOptions: string[] = ["FullTime", "Scholarship", "OSAP"]
 
-  const pi = 3.14
-
-  const toggleCampusSelection = (campus: string) => {
-    setSelectedCampuses((prev) => prev.includes(campus) // check if the toggled campus exists in the selected array
-    ? prev.filter((c) => c !== campus) // if yes, remove it
-    : [...prev, campus] // otherwise add it
-  )  
+  const setCampuses = (campus: string) => {
+    setSelectedCampuses((prev) =>
+      prev.includes(campus) ?
+      prev.filter((c) => c != campus) :
+      [...prev, campus]
+    )
   }
 
-  // UI
-  return (
-    <View style={
-      {
-        flex: 1,
-        alignItems: "center",
-      }}
+  return <>
+  <View style={{
+    flex: 1,
+    alignItems: "center"
+  }}>
+    <Text
+    style={{
+      textAlign: "center",
+      backgroundColor: "#ff9f43",
+      width: "100%",
+      paddingVertical: 10,
+      fontSize: 24, 
+      fontWeight: "bold",
+      color: "#FFF"
+    }}
     >
-      <Text style={{
-        textAlign: 'center',
-        backgroundColor: '#ff9f43',
-        width: '100%',
-        paddingVertical: 10,
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: "#FFF"
-      }}>User Information</Text>
+      User Information
+    </Text>
+    <Image 
+      style={{
+        width: 400,
+        height: 200,
+        marginTop: 20,
+        borderRadius: 15,
+      }}
+      source={{
+        uri: "https://cdn.pixabay.com/photo/2015/09/05/20/02/coding-924920_1280.jpg"
+      }}
+    />
 
-      <Image
-        style={{
-          width: 400,
-          height: 200,
-          marginTop: 20,
-          borderRadius: 15
-        }}
-        source={{
-          uri: "https://cdn.pixabay.com/photo/2015/09/05/20/02/coding-924920_1280.jpg"
-        }} />
-
-      <TextInput
-        style={{
-          borderColor: '#222f3e',
-          borderWidth: 1,
-          padding: 10,
-          marginVertical: 10,
-          fontSize: 18,
-          borderRadius: 10,
-          width: "80%"
-        }}
-        value={name}
-        // onChangeText={(inputString: string) => { setName(inputString) }} 
-        onChangeText={setName}
-        placeholder="Enter your name"
-        autoCapitalize="words"
+    <TextInput 
+      style={styles.textBox}
+      placeholder="Enter your name"
+      value={name}
+      onChangeText={setName}
+      autoCapitalize="words"
+    />
+    <TextInput 
+      style={styles.textBox}
+      value={email}
+      // onChangeText={(inputString: string) => { setName(inputString) }} 
+      onChangeText={setEmail}
+      placeholder="Enter email address"
+      autoCapitalize="none"
+      maxLength={50}
+      autoComplete="email"
+      autoCorrect={false}
+      keyboardType="email-address"
+    />
+    <TextInput 
+      style={styles.textBox}
+      placeholder="Enter password"
+      value={password}
+      onChangeText={setPassword}
+      autoCapitalize="none"
+      maxLength={15}
+      autoCorrect={false}
+      secureTextEntry={true}
       />
 
-      <TextInput
-        style={{
-          borderColor: '#222f3e',
-          borderWidth: 1,
-          padding: 10,
-          marginVertical: 10,
-          fontSize: 18,
-          borderRadius: 10,
-          width: "80%"
-        }}
-        value={email}
-        // onChangeText={(inputString: string) => { setName(inputString) }} 
-        onChangeText={setEmail}
-        placeholder="Enter email address"
-        autoCapitalize="none"
-        maxLength={50}
-        autoComplete="email"
-        autoCorrect={false}
-        keyboardType="email-address"
-      />
+    <Text style={{
+      alignSelf: 'flex-start',
+      fontSize: 18,
+      fontWeight: '500',
+      color: '#576574',
+      marginVertical: 10
+    }}>Preferred Campuses</Text>
 
-      <TextInput
-        style={{
-          borderColor: '#222f3e',
-          borderWidth: 1,
-          padding: 10,
-          marginVertical: 10,
-          fontSize: 18,
-          borderRadius: 10,
-          width: "80%"
-        }}
-        value={password}
-        // onChangeText={(inputString: string) => { setName(inputString) }} 
-        onChangeText={setPassword}
-        placeholder="Enter password"
-        autoCapitalize="none"
-        maxLength={15}
-        autoComplete="off"
-        autoCorrect={false}
-        secureTextEntry={true}
-      />
+    <Dropdown 
+      style={styles.dropdown}
+      data={programs.map((item) => ({value: item, label: item}))}
+      valueField="value"
+      labelField="label"
+      placeholder="Select Program"
+      value={selectedProgram}
+      onChange={(program) => setSelectedProgram(program)}
+    />
 
-      <Text style={{
-        alignSelf: 'flex-start',
-        fontSize: 18,
-        fontWeight: '500',
-        color: '#576574'
-      }}>Select Program</Text>
-      <Dropdown
-        style={{
-          height: 40,
-          width: '80%',
-          borderWidth: 2,
-          borderColor: '#576574',
-          borderRadius: 10,
-          paddingHorizontal: 10
-        }}
-        data={programs.map((prog) => ({ label: prog, value: prog }))}
-        labelField="label"
-        valueField="value"
-        placeholder="Select Program"
-        value={selectedProgram}
-        onChange={(item) => { setSelectedProgram(item.value) }} />
+    <Text style={{
+      alignSelf: 'flex-start',
+      fontSize: 18,
+      fontWeight: '500',
+      color: '#576574',
+      marginVertical: 10
+    }}>Preferred Campuses</Text>
 
-      <Text style={{
-        alignSelf: 'flex-start',
-        fontSize: 18,
-        fontWeight: '500',
-        color: '#576574',
-        marginVertical: 10
-      }}>Preferred Campuses</Text>
+    <View>
       {
         campusOptions.map((campus) => (
-          <View key={campus} style={{
-            width: '60%',
-            flexDirection: 'row',
-            gap: 10,
-            alignItems: 'center'
-          }}>
-            <Checkbox
+          <View 
+            style={styles.checkBox}
+            key={campus}
+          >
+            <Checkbox 
               value={selectedCampuses.includes(campus)}
-              onValueChange={() => {
-                toggleCampusSelection(campus)
-              }}
-              color={selectedCampuses.includes(campus) ? "#54a0ff" : undefined }
+              onValueChange={() => setCampuses(campus)}
+              color={selectedCampuses.includes(campus) ? "#54a0ff" : undefined}
             />
             <Text>{campus}</Text>
           </View>
         ))
       }
     </View>
-  );
+
+  </View>
+  </>
 }
+
+
+const styles = StyleSheet.create({
+  textBox: {
+    borderColor: "#222f3e",
+    borderWidth: 1,
+    padding: 10,
+    marginVertical: 10,
+    fontSize: 18,
+    borderRadius: 10,
+    width: "80%"
+  },
+  dropdown: {
+    height: 40,
+    width: "80%",
+    borderWidth: 2,
+    borderColor: "#576574",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+  },
+  checkBox: {
+    width: "60%",
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+  }
+})
