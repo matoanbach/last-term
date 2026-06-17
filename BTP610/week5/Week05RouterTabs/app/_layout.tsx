5df4fdb109e5b4cd18e775cf9338c2978bdd2d42
matoanbach <bachmatoan2910@gmail.com>
2026-06-16 22:49:21 -0400

    week5: normalize root layout component

--- BTP610/week5/Week05RouterTabs/app/_layout.tsx
+++ BTP610/week5/Week05RouterTabs/app/_layout.tsx
@@ -1,9 +1,10 @@
 import { Stack } from "expo-router";
+import React from "react";
 
-export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
+export default function Layout() {
+    return (
+        <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+    )
 }
